fb59116adb109cc8852683ebf1b61f53bc18dbc5
rayjackson13 <rayjackson@live.ru>
2022-12-13 17:05:32 +0300

    Initial commit.

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,25 +3,25 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Fit Cake`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://fit-cake.vercel.app`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-sass", {
-    resolve: 'gatsby-plugin-google-analytics',
-    options: {
-      "trackingId": ""
+  plugins: [
+    "gatsby-plugin-sass", 
+    "gatsby-plugin-mdx", 
+    "gatsby-transformer-remark",
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "pages",
+        "path": "./src/pages/"
+      },
+      __key: "pages"
     }
-  }, "gatsby-plugin-mdx", "gatsby-transformer-remark", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }]
+  ]
 };
 
 export default config;
